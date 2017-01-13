2ec9d4a07488d11f63fabdfc61aa89c520835cf3
Nick Bourdakos <bourdakos1@gmail.com>
2017-01-13 09:41:59 -0800

    git ignore

---
+++ iOS/Candy-Machine/Candy-Machine/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Candy-Machine
+//
+//  Created by Nicholas Bourdakos on 1/12/17.
+//  Copyright © 2017 Nicholas Bourdakos. All rights reserved.
+//
+
+import UIKit
+import SpeechToTextV1
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var speech: UILabel!
+    @IBOutlet weak var record: UIButton!
+    
+    
+    func startStreaming() {
+        var settings = RecognitionSettings(contentType: .opus)
+        settings.continuous = true
+        settings.interimResults = true
+        let failure = { (error: Error) in print(error) }
+        speechToText.recognizeMicrophone(settings: settings, failure: failure) { results in
+            print(results.bestTranscript)
+        }
+    }
+    
+    func stopStreaming() {
+        speechToText.stopRecognizeMicrophone()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        record.addTarget(self, action: #selector(ViewController.buttonDown(sender:)), for: .touchDown)
+        record.addTarget(self, action: #selector(ViewController.buttonUp(sender:)), for: [.touchUpInside, .touchUpOutside])
+    }
+    
+    func buttonDown(sender: AnyObject) {
+        startStreaming()
+    }
+    
+    func buttonUp(sender: AnyObject) {
+        stopStreaming()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
