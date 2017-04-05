752dd1551c3005121a88616ccdcb2904ae392990
Nick Bourdakos <bourdakos1@gmail.com>
2017-04-05 16:17:56 -0700

    update android for tutorial

--- android/app/src/main/java/com/bourdakos1/candymachine/MainActivity.java
+++ android/app/src/main/java/com/bourdakos1/candymachine/MainActivity.java
@@ -6,20 +6,15 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.ibm.watson.developer_cloud.android.library.audio.AmplitudeListener;
 import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
 import com.ibm.watson.developer_cloud.android.library.audio.utils.ContentType;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
@@ -27,18 +22,12 @@ import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.RecognizeCallback;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private float mLastLevel = 0;
-    private Thread mThread;
-    private static final int SAMPLE_DELAY = 300;
-
-    private ImageView mImageView;
-    private ImageView mImageView2;
     private ImageButton mRecord;
     private TextView mSpeech;
 
@@ -47,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
     private SpeechToText speechService;
 
     private MicrophoneInputStream capture;
-    private boolean listening = false;
+
+    private boolean streaming = false;
+    private boolean waiting = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,13 +47,8 @@ public class MainActivity extends AppCompatActivity {
 
         mRecord = (ImageButton) findViewById(R.id.record);
         mSpeech = (TextView) findViewById(R.id.speech);
-        mImageView = (ImageView) findViewById(R.id.level);
-        mImageView2 = (ImageView) findViewById(R.id.level2);
+        mRecord.setBackground(getDrawable(R.drawable.watson_blue));
 
-        mImageView.animate().scaleX(0).setDuration(0);
-        mImageView.animate().scaleY(0).setDuration(0);
-        mImageView2.animate().scaleX(0).setDuration(0);
-        mImageView2.animate().scaleY(0).setDuration(0);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
@@ -70,84 +56,55 @@ public class MainActivity extends AppCompatActivity {
 
         speechService = initSpeechToTextService();
 
-        mRecord.setOnTouchListener(new View.OnTouchListener() {
+        mRecord.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    // This is needed because it will change by the time the request is made
-                    final String text = mSpeech.getText().toString();
+            public void onClick(View v) {
+                if (!streaming && !waiting) {
                     mRecord.setBackground(getDrawable(R.drawable.watson_red));
-                    RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                    StringRequest sr = new StringRequest(Request.Method.POST,"http://candy-machine.mybluemix.net/sentiment", new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            Log.d(TAG, response);
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-
-                        }
-                    }){
-                        @Override
-                        protected Map<String,String> getParams(){
-                            Map<String,String> params = new HashMap<>();
-                            params.put("transcript", text);
-                            return params;
-                        }
-
-                        @Override
-                        public Map<String, String> getHeaders() throws AuthFailureError {
-                            Map<String,String> params = new HashMap<>();
-                            params.put("Content-Type","application/x-www-form-urlencoded");
-                            return params;
-                        }
-                    };
-                    queue.add(sr);
-
-                    mSpeech.setText("Tap & Hold");
-                    mSpeechFirstPart = "";
-                    listening = false;
-                    try {
-                        capture.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    return true;
-                } else {
+                    streaming = true;
+                    startStreaming();
+                    mSpeech.setText("Listening...");
+                } else if (!waiting) {
                     mRecord.setBackground(getDrawable(R.drawable.watson_blue));
-                    if(!listening) {
-                        listening = true;
-                        capture = new MicrophoneInputStream(true);
-                        capture.setOnAmplitudeListener(new AmplitudeListener() {
-                            @Override
-                            public void onSample(double amplitude, double volume) {
-                                Log.d(TAG, "amp: " + amplitude + "vol:" + volume);
-//                                if (volume > mImageView.getScaleX()) {
-//                                    mImageView.setScaleX(volume);
-//                                    mImageView.setScaleY(volume);
-//                                    mImageView2.setScaleX(volume);
-//                                    mImageView2.setScaleY(volume);
-//                                    mImageView.animate().scaleX(0).scaleY(0).setDuration(600);
-//                                    mImageView2.animate().scaleX(0).scaleY(0).setDuration(1000);
-//                                }
-                            }
-                        });
-                        new Thread(new Runnable() {
-                            @Override
-                            public void run() {
-                                try {
-                                    speechService.recognizeUsingWebSocket(capture, getRecognizeOptions(), new MicrophoneRecognizeDelegate());
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }).start();
-                    }
-                    return true;
+                    streaming = false;
+                    waiting = true;
+                    stopStreaming();
                 }
             }
         });
+    }
+
+    private void startStreaming() {
+        capture = new MicrophoneInputStream(true);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    speechService.recognizeUsingWebSocket(
+                            capture,
+                            new RecognizeOptions.Builder()
+                                    .continuous(true)
+                                    .contentType(ContentType.OPUS.toString())
+                                    .model("en-US_BroadbandModel")
+                                    .interimResults(true)
+                                    .inactivityTimeout(2000)
+                                    .build(),
+                            new MicrophoneRecognizeDelegate()
+                    );
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void stopStreaming() {
+        // This is needed because it will change by the time the request is made
+        try {
+            capture.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private SpeechToText initSpeechToTextService() {
@@ -160,16 +117,6 @@ public class MainActivity extends AppCompatActivity {
         return service;
     }
 
-    private RecognizeOptions getRecognizeOptions() {
-        return new RecognizeOptions.Builder()
-                .continuous(true)
-                .contentType(ContentType.OPUS.toString())
-                .model("en-US_BroadbandModel")
-                .interimResults(true)
-                .inactivityTimeout(2000)
-                .build();
-    }
-
     private class MicrophoneRecognizeDelegate implements RecognizeCallback {
         @Override
         public void onTranscription(SpeechResults speechResults) {
@@ -180,7 +127,11 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        mSpeech.setText(mSpeechFirstPart + text);
+                        if (!(mSpeechFirstPart + text).equals("")) {
+                            mSpeech.setText(mSpeechFirstPart + text);
+                        } else {
+                            mSpeech.setText("Listening...");
+                        }
                         Log.d(TAG, mSpeech.getText().toString());
                         if (isFinal) {
                             mSpeechFirstPart += text;
@@ -200,6 +151,35 @@ public class MainActivity extends AppCompatActivity {
 
         @Override public void onDisconnected() {
             Log.d(TAG, "Disconnected");
+            mSpeechFirstPart = "";
+            final String text = mSpeech.getText().toString();
+            RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+            try {
+                final JSONObject jsonBody = new JSONObject("{\"text\":\"" + text + "\"}");
+                JsonObjectRequest jor = new JsonObjectRequest("http://candy-machine.mybluemix.net/sentiment", jsonBody, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        try {
+                            mSpeech.setText(response.getString("sentiment"));
+                            waiting = false;
+                        } catch (JSONException je) {
+                            je.printStackTrace();
+                            mSpeech.setText("Try again");
+                            waiting = false;
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mSpeech.setText("Try again");
+                        waiting = false;
+                    }
+                });
+                queue.add(jor);
+            } catch (JSONException je) {
+                je.printStackTrace();
+            }
         }
     }
 }
